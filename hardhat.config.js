85ac22bf321b503007ec86ec271a028f7a92dfad
muasya0254 <ephraim.muasya@kibo.school>
2023-09-16 08:55:18 +0300

    configuring hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config({path: '.env'});
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "maratestnet",
+  networks: {
+    maratestnet: {
+      url: "https://testapi.mara.xyz/http",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 123456,
+    },
+  },
+  solidity:{
+    version: "0.8.19", 
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
